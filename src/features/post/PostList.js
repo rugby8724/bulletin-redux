83eaada403565a8ebdf8f19ab088215f8f54cea9
Jeremiah <jeremiah.d.wise@gmail.com>
2022-08-14 09:31:16 -0500

    add create asyncthunk

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -1,36 +1,42 @@
 // useSeletor is a hook to access the redux store's state. This hook takes a selector function as an argument. The selector is called with the store state.
 // This hook takes an optional equality comparison function as the second parameter that allows you to customize the way the selected state is compared to determine whether the component needs to be re-rendered.
 import React from 'react'
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from './postsSlice';
+import { useEffect } from 'react';
+import PostsExcerpt from './PostsExcerpt';
 
-import { selectAllPost } from './postsSlice';
-import PostAuthor from './PostAuthor';
-import TimeAgo from './TimeAgo';
-import ReactionButtons from './ReactionsButtons';
 
 const PostList = () => {
-  const posts = useSelector(selectAllPost)
+  const dispatch = useDispatch()
 
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-  const renderedPosts = orderedPosts.map(post => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0,100)}</p>
+  useEffect(() => {
+    if (postStatus === 'idle') {
+        dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
 
-      <p className='postCredit'>
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  let content;
+  if (postStatus === 'loading') {
+      content = <p>"Loading..."</p>;
+  } else if (postStatus === 'succeeded') {
+      const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+      content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+  } else if (postStatus === 'failed') {
+      content = <p>{error}</p>;
+  }
+
   return (
-    <section>
-      <h2>Posts</h2>
-      {renderedPosts}
-    </section>
+      <section>
+          <h2>Posts</h2>
+          {content}
+      </section>
   )
+
 }
 
 export default PostList
--- src/features/post/postsSlice.js
+++ src/features/post/postsSlice.js
@@ -1,80 +1,108 @@
-// A function that accepts an initial state, an object full of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state.
-
-
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
+import axios from "axios";
 
-const initialState = [
-  {
-      id: '1',
-      title: 'Learning Redux Toolkit',
-      content: "I've heard good things.",
-      date: sub(new Date(), { minutes: 10 }).toISOString(),
-      reactions: {
-          thumbsUp: 0,
-          wow: 0,
-          heart: 0,
-          rocket: 0,
-          coffee: 0
-      }
-  },
-  {
-      id: '2',
-      title: 'Slices...',
-      content: "The more I say slice, the more I want pizza.",
-      date: sub(new Date(), { minutes: 5 }).toISOString(),
-      reactions: {
-          thumbsUp: 0,
-          wow: 0,
-          heart: 0,
-          rocket: 0,
-          coffee: 0
-      }
-  }
-]
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
-// Redux Toolkit's createReducer and createSlice automatically use Immer internally to let you write simpler immutable update logic using "mutating" syntax. This helps simplify most reducer implementations.
-const postsSlice = createSlice({
-  name: 'posts',
-  initialState,
-  reducers:{
-    postAdded: { 
-      reducer(state, action){
-      state.push(action.payload)
-      },
-      prepare(title, content, userId){
-        return {
-          payload: {
-            id: nanoid(),
-            title,
-            content,
-            date: new Date().toISOString(), 
-            userId,
-            reactions: {
-              thumbsUp: 0,
-              wow: 0,
-              heart: 0,
-              rocket: 0,
-              coffee: 0
-            }
-          }
-        }
-      }
-    },
-    reactionAdded(state, action) {
-      const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
-      if (existingPost) {
-          existingPost.reactions[reaction]++
-      }
-  }
-  }
+const initialState = {
+    posts: [],
+    status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await axios.get(POSTS_URL)
+    return response.data
 })
 
-export const selectAllPost = (state) => state.posts
+export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost)
+    return response.data
+})
 
-// when we create reducer functions, create slice automatically genterates an action creator function with the same name 
-// when we export our action, we are actually exporting the action creator function
-export const {postAdded, reactionAdded} = postsSlice.actions
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded: {
+            reducer(state, action) {
+                state.posts.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        date: new Date().toISOString(),
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
+                    }
+                }
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.posts.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
+            }
+        }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                // Adding date and reactions
+                let min = 1;
+                const loadedPosts = action.payload.map(post => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                });
+
+                // Add any fetched posts to the array
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                action.payload.userId = Number(action.payload.userId)
+                action.payload.date = new Date().toISOString();
+                action.payload.reactions = {
+                    thumbsUp: 0,
+                    hooray: 0,
+                    heart: 0,
+                    rocket: 0,
+                    eyes: 0
+                }
+                console.log(action.payload)
+                state.posts.push(action.payload)
+            })
+    }
+})
+
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
+
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
